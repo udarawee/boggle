7f05efa632963eebf2b26345e0fece64bdb7f031
udara <udara.weera@gmail.com>
2020-04-10 13:54:01 -0400

    boggle dictionary to check word validity

--- app/services/boggle_dictionary.rb
+++ app/services/boggle_dictionary.rb
@@ -1,5 +1,15 @@
 class BoggleDictionary
-  def self.exists?(word)
-    Rails.cache.fetch(word.downcase).present?
+  PREFIX_VALUE = 1
+  WORD_VALUE = 2
+  def self.word?(word)
+    word.length > 2 && entry(word) == WORD_VALUE
+  end
+
+  def self.prefix?(word)
+    entry(word) >= PREFIX_VALUE
+  end
+
+  def self.entry(word)
+    Rails.cache.fetch(word.downcase) || 0
   end
 end
